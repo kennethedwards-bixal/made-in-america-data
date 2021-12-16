2fd81c5911c7a498866b8b0a919483f806d45beb
kedwards-a <kedwards-a@kedwards-l1.fios-router.home>
2021-12-16 16:34:13 -0500

    edited script to keep current running log of waivers

--- buildwaiverdata.js
+++ buildwaiverdata.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const fs = require('fs')
 const axios = require('axios');
 const dataDir = '.';
